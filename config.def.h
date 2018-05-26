7d5d70a08c0a311978a77fb704720134983c9245
MacRat <m@crat.jp>
2018-05-26 22:59:48 +0900

    Added screen lock command

--- config.def.h
+++ config.def.h
@@ -58,6 +58,7 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_accent, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
+static const char *lockcmd[] = { "xscreensaver-command", "-lock" };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
@@ -93,6 +94,7 @@ static Key keys[] = {
 	TAGKEYS(                        XK_7,                      6)
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
+	{ MODKEY|ShiftMask,             XK_l,      spawn,          {.v = lockcmd } },
 	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
 };
 
