87d14b890556bad0d89bb1caff91c8e925d9fc0e
MacRat <m@crat.jp>
2020-08-22 22:29:46 +0900

    Changed default settings of xtile

--- config.def.h
+++ config.def.h
@@ -85,12 +85,12 @@ static Key keys[] = {
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[2]} },
 	{ MODKEY,                       XK_space,  setlayout,      {0} },
-	TILEKEYS(MODKEY,                                           1, 0, 0)
-	TILEKEYS(MODKEY|ShiftMask,                                 0, 1, 0)
-	TILEKEYS(MODKEY|ControlMask,                               0, 0, 1)
-	TILEKEYS(MODKEY|ShiftMask|ControlMask,                     1, 1, 1)
+	TILEKEYS(MODKEY,                                           1, 1, 1)
+	TILEKEYS(MODKEY|ShiftMask,                                 1, 0, 0)
+	TILEKEYS(MODKEY|ControlMask,                               0, 1, 0)
+	TILEKEYS(MODKEY|ShiftMask|ControlMask,                     0, 0, 1)
 	{ MODKEY|ShiftMask,             XK_t,      setdirs,        {.v = (int[]){ DirHor, DirVer, DirVer } } },
-	{ MODKEY|ControlMask,           XK_t,      setdirs,        {.v = (int[]){ DirVer, DirHor, DirHor } } },
+	{ MODKEY|ControlMask,           XK_t,      setdirs,        {.v = (int[]){ DirRotVer, DirHor, DirHor } } },
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
 	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
